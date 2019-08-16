432de1993df421312c5aa24678442bf59991015a
guominS <52800170+guominS@users.noreply.github.com>
2019-08-16 13:06:56 -0400

    Add files via upload

---
+++ assets/data/speakerData.ts
@@ -0,0 +1,11 @@
+import {Speaker} from '../../app/speaker'
+export const SPEAKERDATA:Speaker =  
+    {
+        Style: 'styleA',
+        Color:'pink',
+        Brand: 'brandA'}
+    // export const SPEAKERDATA:Speaker[] =  [
+    //     {Style: 'styleA',Color:'pink',Brand: 'brandA'},
+    //     {Style: 'styleB',Color:'blue',Brand: 'brandB'},
+    //     {Style: 'styleC',Color:'red',Brand: 'brandC'},
+    //     ]
